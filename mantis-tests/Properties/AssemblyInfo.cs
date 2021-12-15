a1badfd75bbfaf8077b686f81ab7115f13d639dd
Aigul Shakirova <aigul.shakirova@gmail.com>
2021-12-15 18:59:26 +0300

    Реализован тест для создания новой учётной записи в Mantis.

---
+++ mantis-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mantis-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mantis-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("542cc4a8-f381-48af-92cd-ca9b96a5515a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mantis-tests/tests/AccountCreationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace mantis_tests 
+{
+    
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [OneTimeSetUp]
+        public void SetUpConfig()
+        {
+            app.Ftp.BackupFile("/config_inc.php"); // @"D:\xampp\htdocs\mantisbt-2.25.2\config"
+            using (Stream localFile = File.Open("config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("/config_inc.php", localFile); // @"D:\xampp\htdocs\mantisbt-2.25.2\config"
+            };
+            
+        }
+
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData() {
+                Name = "testuser",
+                Password = "password",
+                Email = "testuser@localhost.localdomain"
+            };
+
+            app.Registration.Register(account);
+        }
+
+        [OneTimeTearDown]
+        public void RestoreConfig()
+        {
+            app.Ftp.RestoreBackupFile("/config_inc.php");
+        }
+    }
+}
